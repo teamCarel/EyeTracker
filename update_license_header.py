ddaba2a4eeb317e06e96fb3f1611575473bacfd0
Kateřina Kopřivová <slepyce@gmail.com>
2017-05-16 10:08:19 +0200

    Update update_license_header.py

--- update_license_header.py
+++ update_license_header.py
@@ -5,7 +5,12 @@ Copyright (C) 2012-2017  Pupil Labs
 
 Distributed under the terms of the GNU
 Lesser General Public License (LGPL v3.0).
-See COPYING and COPYING.LESSER for license details.
+ See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~
+
+-------------!!!!!  IMPORTANT !!!!---------------------------------------
+For correct compilling connect Windows dependecies from:
+https://docs.pupil-labs.com/#windows-dependencies
 ---------------------------------------------------------------------------~(*)
 '''
 import fnmatch
